081016b38f32e317299d04bf9ce162ce1af581fd
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-05-02 17:23:28 +0100

    :rotating_light: :sparkles: getting test files

--- bin/runner.py
+++ bin/runner.py
@@ -10,3 +10,11 @@ def get_test_directories(top):
 def add_directory_paths(array):
     for path in array:
         sys.path.append(path)
+
+def get_test_files(directory):
+    test_filenames = []
+    for (dirpath, dirnames, filenames) in os.walk(directory):
+        for filename in filenames:
+            if filename[0:5] == "test_" and filename[-3:] == ".py":
+                test_filenames.append(filename[0:-3])
+    return test_filenames
--- bin/tests/test_runner.py
+++ bin/tests/test_runner.py
@@ -4,7 +4,7 @@ from context import *
 print('return relative paths of those directories named /tests')
 
 directory_path = os.path.dirname(os.path.realpath(__file__))
-test_directory = directory_path + '/example/fizz_buzz/tests'
+test_directory = directory_path + '/example/fizzbuzz/tests'
 
 file_paths = get_test_directories(directory_path)
 
@@ -14,3 +14,10 @@ print('add directory_paths to sys.path')
 add_directory_paths(['hello'])
 
 print(Expect(sys.path).to_include('hello'))
+
+print('get files/modules in test directory')
+
+test_directory = directory_path + '/example/fizzbuzz/tests'
+test_files = get_test_files(test_directory)
+
+print(Expect(['test_printer','test_logic']).to_equal(test_files))

cf60ff7d6750e704a29d458376083dbe12094925
AlexMcCarroll <alexandra@mccarrollhk.com>
2018-05-02 16:31:48 +0100

    :rotating_light: Add tests for to_include method

--- bin/tests/test_runner.py
+++ bin/tests/test_runner.py
@@ -1,9 +1,8 @@
-# return relative paths of those directories
-import os
+import os, sys
 from context import Expect
 from context import get_test_directories
 
-# print(os.path.dirname('.'))
+# return relative paths of those directories
 
 directory_path = os.path.dirname(os.path.realpath(__file__))
 test_directory = directory_path + '/example/fizz_buzz/tests'
@@ -11,3 +10,9 @@ test_directory = directory_path + '/example/fizz_buzz/tests'
 file_paths = get_test_directories(directory_path)
 
 print(Expect([directory_path,test_directory]).to_equal(file_paths))
+
+# add directory_paths to sys.path
+
+# add_directory_paths(['hello'])
+#
+# print(Expect(sys.path).to_include('hello'))
--- src/tests/test_expect.py
+++ src/tests/test_expect.py
@@ -13,3 +13,18 @@ print(expect.to_equal(3)["result"] == False)
 
 print("to_equal matcher false reason")
 print(expect.to_equal(3)["reason"] == "Expected: 2\nGot: 3")
+
+expect_2 = Expect(['hello'])
+
+print("to_include matcher(true)")
+print(expect_2.to_include('hello')["result"] == True)
+
+expect_3 = Expect("hello")
+
+print "to_include matcher(not a list)"
+print(expect_3.to_include("hello")["result"] == False)
+
+expect_4 = Expect([])
+
+print "to_include matcher(not included)"
+print(expect_4.to_include("hello")["result"] == False)

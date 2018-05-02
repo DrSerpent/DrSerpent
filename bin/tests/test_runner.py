61a934d1bf116e69f5c5072a33b86b2bcc2b7acd
AlexMcCarroll <alexandra@mccarrollhk.com>
2018-05-02 16:55:35 +0100

    :hammer: names added to tests

--- bin/tests/test_runner.py
+++ bin/tests/test_runner.py
@@ -1,8 +1,7 @@
 import os, sys
-from context import Expect
 from context import *
 
-# return relative paths of those directories
+print('return relative paths of those directories named /tests')
 
 directory_path = os.path.dirname(os.path.realpath(__file__))
 test_directory = directory_path + '/example/fizz_buzz/tests'
@@ -11,8 +10,7 @@ file_paths = get_test_directories(directory_path)
 
 print(Expect([directory_path,test_directory]).to_equal(file_paths))
 
-# add directory_paths to sys.path
-
+print('add directory_paths to sys.path')
 add_directory_paths(['hello'])
 
 print(Expect(sys.path).to_include('hello'))

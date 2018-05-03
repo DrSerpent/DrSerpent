4bd91cc5d6c7b8de1ce9b523d760ab3d0c60ee40
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-05-04 00:04:59 +0100

    made failing tests fail

--- bin/tests/example_projects/broken_fizzbuzz/example_tests/test_broken_logic.py
+++ bin/tests/example_projects/broken_fizzbuzz/example_tests/test_broken_logic.py
@@ -2,13 +2,13 @@ import os, sys
 from broken_fizzbuzz_context import *
 
 def test_broken_fizz():
-    return Expect(Fizzbuzz.run(3)).to_equal('Fizz')
+    return Expect(Fizzbuzz.run(3)).to_equal('HEY')
 
 def test_broken_buzz():
-    return Expect(Fizzbuzz.run(5)).to_equal('Buzz')
+    return Expect(Fizzbuzz.run(5)).to_equal('YES')
 
 def test_broken_fizzbuzz():
-    return Expect(Fizzbuzz.run(15)).to_equal('Fizzbuzz')
+    return Expect(Fizzbuzz.run(15)).to_equal('NO')
 
 def test_broken_number():
-    Expect(Fizzbuzz.run(1)).to_equal(1)
+    Expect(Fizzbuzz.run(1)).to_equal(1 + 1)

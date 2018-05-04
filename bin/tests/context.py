a97361e389c91e1d38692b20c50f80e0ef3f2054
Tom Betts <tom.betts@live.co.uk>
2018-05-04 15:38:33 +0100

    change /src to /drserpent in context files

--- bin/tests/context.py
+++ bin/tests/context.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('.') + "/bin")
-sys.path.insert(0, os.path.abspath('.') + "/src")
+sys.path.insert(0, os.path.abspath('.') + "/drserpent")
 
 from expect import Expect
 from collector import *

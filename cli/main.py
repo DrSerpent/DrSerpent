3a8947209baea669bd35c39bb2a2cfbea599b6b7
Hemesh-Unka <hemeshunka@gmail.com>
2018-05-06 11:20:14 +0100

    :sparkles::hammer::hammer: - --init command now implemented, removed init cli command

--- cli/main.py
+++ cli/main.py
@@ -1,18 +1,30 @@
 import click
+import os
+
 from .about import about
-from .init import init
 from .test import test
 from testrunner import run
 
 @click.group(invoke_without_command=True, no_args_is_help=False)
-@click.version_option(version=None, help='Display the current version.', message='Serpent %(version)s')
 @click.pass_context
-def cli(ctx):
-    if ctx.invoked_subcommand is None:
+@click.version_option(version=None, help='Display the current version.', message='Serpent %(version)s')
+@click.option('--init', help='Initialize your project with Serpent.', is_flag=True)
+def cli(ctx, init):
+    # --init
+    if ctx.invoked_subcommand is None and init:
+        if not os.path.exists('tests'):
+            os.makedirs('tests')
+            click.echo('    created  test/')
+        else:
+            click.echo('    exists   test/')
+
+    # --run tests
+    elif ctx.invoked_subcommand is None:
         run()
+
+    # get subcommands
     else:
         pass
 
 cli.add_command(about)
-cli.add_command(init)
 cli.add_command(test)

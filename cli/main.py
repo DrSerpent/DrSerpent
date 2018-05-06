3aff728cb70240eda6ffea132effd075313e9598
Hemesh-Unka <hemeshunka@gmail.com>
2018-05-06 11:03:00 +0100

    :sparkles: intergrated version command for cli usage

--- cli/main.py
+++ cli/main.py
@@ -4,7 +4,8 @@ from .init import init
 from .test import test
 from testrunner import run
 
-@click.group(invoke_without_command=True)
+@click.group(invoke_without_command=True, no_args_is_help=False)
+@click.version_option(version=None, help='Display the current version.', message='Serpent %(version)s')
 @click.pass_context
 def cli(ctx):
     if ctx.invoked_subcommand is None:

82f9bfbbe47194d8e474ee047b5e749543d3d715
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-05-07 11:40:53 +0100

    :hammer: fixed file paths for failing tests

--- tests/example_projects/broken_fizzbuzz/example_tests/broken_fizzbuzz_context.py
+++ tests/example_projects/broken_fizzbuzz/example_tests/broken_fizzbuzz_context.py
@@ -1,8 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.') + "/bin/tests/example_projects/broken_fizzbuzz")
-sys.path.insert(0, os.path.abspath('.') + "/drserpent")
+sys.path.insert(0, os.path.abspath('.') + "/tests/example_projects/broken_fizzbuzz")
 
 from expect import Expect
 from broken_logic import *

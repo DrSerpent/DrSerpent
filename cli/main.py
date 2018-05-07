e5582be8e20e2f445d47359f414634f48e5facf2
Hemesh-Unka <hemeshunka@gmail.com>
2018-05-07 12:15:37 +0100

    :art: improving code structure

--- cli/main.py
+++ cli/main.py
@@ -5,10 +5,21 @@ from .about import about
 from .test import test
 from testrunner import run
 
-@click.group(invoke_without_command=True, no_args_is_help=False)
+@click.group(
+    invoke_without_command=True,
+    no_args_is_help=False)
 @click.pass_context
-@click.version_option(version=None, help='Display the current version.', message='Serpent %(version)s')
-@click.option('--init', help='Initialize your project with Serpent.', is_flag=True)
+
+@click.version_option(
+    version=None,
+    help='Display the current version.',
+    message='Serpent %(version)s')
+
+@click.option('
+--init',
+help='Initialize your project with Serpent.',
+is_flag=True)
+
 def cli(ctx, init):
     # --init
     if ctx.invoked_subcommand is None and init:

2f09ff701274eb1658447c2d1133e76cf14eed68
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-05-07 15:55:14 +0100

    :sparkles: :rotating_light: spy created

--- tests/test_spy.py
+++ tests/test_spy.py
@@ -2,10 +2,35 @@ from context import Expect
 from spy import Spy
 
 def test_spy_on_returns_a_callable_object():
-    spy = Spy(6)
+    def hello(name):
+        print(f'hello {name}')
+    spy = Spy(hello)
     return Expect(callable(spy)).to_equal(True)
 
-def test_spy_on_returns_a_callable_object():
+def test_spy_stores_original_function():
+    def hello(name):
+        print(f'hello {name}')
+    spy = Spy(hello)
+    return Expect(spy.original).to_equal(hello)
+
+# Throw Error Test
+#
+# def test_spy_can_only_be_used_on_a_function():
+#     def hello(name):
+#         print(f'hello {name}')
+#     spy = Spy(hello)
+#     return Expect(callable(spy)).to_equal(True)
+
+def test_spy_knows_if_it_has_not_been_called():
+    spy = Spy(6)
+    return Expect(spy.called).to_equal(False)
+
+def test_spy_knows_if_it_has_been_called():
+    spy = Spy(6)
+    spy()
+    return Expect(spy.called).to_equal(True)
+
+def test_spy_counts_number_of_calls():
     spy = Spy(6)
     spy()
     return Expect(spy.called).to_equal(True)

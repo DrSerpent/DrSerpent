19597a222a0278d45f10ced2fe13203cbed147fd
Hemesh-Unka <hemeshunka@gmail.com>
2018-05-08 14:26:05 +0100

    test: example cli command feature implemented with tests

--- cli/example.py
+++ cli/example.py
@@ -1,4 +1,10 @@
-import click
+import click, os, shutil
+
+
+TEST_SRC_FILE = os.path.dirname(__file__) + '/../tests/example_projects/fizzbuzz/example_tests/test_logic.py'
+TEST_DST_ROOT = './tests'
+SRC_FILE = os.path.dirname(__file__) + '/../tests/example_projects/fizzbuzz/logic.py'
+DST_ROOT = '.'
 
 @click.group()
 def example():
@@ -6,4 +12,22 @@ def example():
 
 @example.command()
 def example():
-    click.echo("Initializing some examples")
+    click.echo("Initialising fizzbuzz example")
+    create_test_dir()
+    create_fizzbuzz_file()
+    create_fizzbuzz_test_file()
+
+def create_test_dir():
+    if not os.path.exists('tests'):
+        os.makedirs('tests')
+        click.echo('\t\tcreated\ttest/')
+    else:
+        click.echo('\t\texists\ttest/')
+
+def create_fizzbuzz_test_file():
+    if not os.path.isfile('tests/test_logic.py'):
+        shutil.copy(TEST_SRC_FILE, TEST_DST_ROOT)
+
+def create_fizzbuzz_file():
+    if not os.path.isfile('logic.py'):
+        shutil.copy(SRC_FILE, DST_ROOT)
--- tests/context.py
+++ tests/context.py
@@ -5,3 +5,4 @@ from expect import Expect
 from collector import *
 from executor import *
 from cli.about import *
+from cli.example import *

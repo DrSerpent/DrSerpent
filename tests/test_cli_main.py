44e8c58015b089cf01b6c4e4604829148eb69767
Hemesh-Unka <hemeshunka@gmail.com>
2018-05-08 14:53:26 +0100

    test: cli init command creates an empty test folder

--- tests/test_cli_main.py
+++ tests/test_cli_main.py
@@ -14,3 +14,26 @@ def test_version_CLI_should_print_current_version():
     result = runner.invoke(cli, ['--version'])
 
     return Expect(result.output).to_equal('Serpent 1.0\n')
+
+def test_init_CLI_command_exit_code_should_equal_zero():
+    runner = CliRunner()
+    with runner.isolated_filesystem() as tempdir:
+        result = runner.invoke(cli, ['--init'])
+
+    return Expect(result.exit_code).to_equal(0)
+
+def test_init_CLI_command_should_create_a_test_folder():
+    runner = CliRunner()
+    with runner.isolated_filesystem() as tempdir:
+        runner.invoke(cli, ['--init'])
+        result = os.path.isdir(tempdir + '/tests')
+
+    return Expect(result).to_equal(True)
+
+# def test_init_CLI_command_should_create_a_test_folder_with_a_context_file():
+#     runner = CliRunner()
+#     with runner.isolated_filesystem() as tempdir:
+#         runner.invoke(cli, ['--init'])
+#         result = os.path.isdir(tempdir + '/tests.context.py')
+#
+#     return Expect(result).to_equal(True)

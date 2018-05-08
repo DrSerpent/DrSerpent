77ce18752a4b139f6ca4932d279b0f9d5ef4016a
Hemesh-Unka <hemeshunka@gmail.com>
2018-05-08 14:30:02 +0100

    test: renaming file name

---
+++ tests/test_cli_main.py
@@ -0,0 +1,6 @@
+# def test_cli_version():
+#     runner = CliRunner()
+#     result = runner.invoke(cli.main.cli, ['--version'])
+#     assert result.exit_code == 0
+#     assert result.output == "shakedown, version " + shakedown.VERSION + "\n"
+#     print(result.output)

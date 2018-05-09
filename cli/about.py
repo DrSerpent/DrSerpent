0a4e64ca08d599671f6cbf965c5afdee93ef0740
Hemesh-Unka <hemeshunka@gmail.com>
2018-05-09 09:46:35 +0100

    fix: fixing merge conflict within about cli command

--- cli/about.py
+++ cli/about.py
@@ -1,9 +1,11 @@
 import click
 
+SNAKE_EMOJI = '\U0001F40D'
+
 @click.group()
 def about():
     pass
 
 @about.command()
 def about():
-    click.echo(emoji.emojize('Made with :snake: by Alexandra McCarroll, Tom Betts, Ricky Hewitt, Hemesh Unka (Feb 2018 Cohort - Makers Academy)'))
+    print(f"Made with {SNAKE_EMOJI} by Alexandra McCarroll, Tom Betts, Richard Hewitt, Hemesh Unka (February 2018 Cohort - Makers Academy)")

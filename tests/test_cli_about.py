6dae635e86ea3a1e4f1e2e7c0748fed89d48cee9
Hemesh-Unka <hemeshunka@gmail.com>
2018-05-10 12:57:57 +0100

    test: fixing tests output

--- tests/test_cli_about.py
+++ tests/test_cli_about.py
@@ -11,4 +11,4 @@ def test_flag_about_CLI_command_output_from_cli_command_about():
     runner = CliRunner()
     result = runner.invoke(cli, ['--about'])
 
-    return Expect(result.output).to_equal('Made with \U0001F40D by Alexandra McCarroll, Tom Betts, Richard Hewitt, Hemesh Unka (February 2018 Cohort - Makers Academy)\n')
+    return Expect(result.output).to_equal('Made with \U0001F40D (Python) by Alexandra McCarroll, Tom Betts, Richard Hewitt, Hemesh Unka (February 2018 Cohort - Makers Academy)\n')
